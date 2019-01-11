891fdb4bb3e6a7792a2621d24336739e520a7e59
Sohel Dhengre <soheldhengre@gmail.com>
2019-01-11 16:43:05 +0530

    Working on LoginVC

---
+++ BitGram/Controller/LoginVC.swift
@@ -0,0 +1,58 @@
+//
+//  LoginVC.swift
+//  BitGram
+//
+//  Created by SD on 11/01/19.
+//  Copyright © 2019 Sohel Dhengre. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    let emailTextField: UITextField = {
+       let tf = UITextField()
+        tf.placeholder = "Email"
+        tf.borderStyle = .roundedRect
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.font = UIFont.systemFont(ofSize: 14)
+        return tf
+    }()
+    
+    let passwordTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Password"
+        tf.borderStyle = .roundedRect
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.font = UIFont.systemFont(ofSize: 14)
+        return tf
+    }()
+    
+    let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Login", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = UIColor(red: 149/255, green: 204/255, blue: 244/255, alpha: 1)
+        button.layer.cornerRadius = 5
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //background Color
+        view.backgroundColor = .white
+        configureViewComponents()
+
+    }
+    
+    func configureViewComponents(){
+        //StackView
+        let stackView = UIStackView(arrangedSubviews: [emailTextField, passwordTextField,loginButton])
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 10
+        view.addSubview(stackView)
+        stackView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 40, paddingLeft: 40, paddingBottom: 0, paddingRight: 40, width: 0, height: 140)
+    }
+    
+}
