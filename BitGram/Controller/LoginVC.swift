4a5e34854cb7209abac060ebe7290d25716908b9
Sohel Dhengre <soheldhengre@gmail.com>
2019-01-12 15:10:31 +0530

    Done with Login Screen UI

--- BitGram/Controller/LoginVC.swift
+++ BitGram/Controller/LoginVC.swift
@@ -9,6 +9,18 @@
 import UIKit
 
 class LoginVC: UIViewController {
+    
+    let logoContainerView: UIView = {
+        let view = UIView()
+        let logoImageView = UIImageView(image: #imageLiteral(resourceName: "Instagram_logo_white"))
+        view.addSubview(logoImageView)
+        logoImageView.anchor(top: nil, left: nil, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 200 , height: 50)
+        logoImageView.contentMode = .scaleAspectFill
+        logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        logoImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        view.backgroundColor = UIColor(displayP3Red: 0/255, green: 120/255, blue: 175/255, alpha: 1)
+        return view
+    }()
 
     let emailTextField: UITextField = {
        let tf = UITextField()
@@ -37,11 +49,23 @@ class LoginVC: UIViewController {
         return button
     }()
     
+    let dontHaveAnAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        let attributedTitle = NSMutableAttributedString(string: "Don't ahve an account?   ", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 14), NSAttributedString.Key.foregroundColor: UIColor.lightGray])
+        attributedTitle.append(NSAttributedString(string: "Sign Up", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 14), NSAttributedString.Key.foregroundColor: UIColor(displayP3Red: 0/255, green: 120/255, blue: 175/255, alpha: 1) ]))
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //background Color
         view.backgroundColor = .white
+        view.addSubview(logoContainerView)
+        logoContainerView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 150)
         configureViewComponents()
+        view.addSubview(dontHaveAnAccountButton)
+        dontHaveAnAccountButton.anchor(top: nil, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 12, paddingRight: 0, width: 0, height: 50)
 
     }
     
@@ -52,7 +76,7 @@ class LoginVC: UIViewController {
         stackView.distribution = .fillEqually
         stackView.spacing = 10
         view.addSubview(stackView)
-        stackView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 40, paddingLeft: 40, paddingBottom: 0, paddingRight: 40, width: 0, height: 140)
+        stackView.anchor(top: logoContainerView.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 40, paddingLeft: 40, paddingBottom: 0, paddingRight: 40, width: 0, height: 140)
     }
     
 }
